8a10d7368690182991f9295b849c4b23d4b7a9a3
Patrick Lumumba <97313939+lumumbapl@users.noreply.github.com>
2024-03-20 20:48:35 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
+/**
+ * onDOMLoaded routines
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.querySelector('.attributes')) {
+        let wcbvCookie = document.cookie;
+
+        wcbvCookie = wcbvCookie.replace(/(?:(?:^|.*;\s*)wcbv\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+        document.getElementById('pa_shows').value = wcbvCookie;
+    }
+});
 
